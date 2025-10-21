bd274205b1346fff53bb36365472e6dbeace36d8
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-10-21 12:37:44 -0300

    feature:criando accounts

--- 5_accounts/index.js
+++ 5_accounts/index.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 
 console.log("Iniciamos o Accounts");
 
+operation();
+
+
 function operation() {
   inquirer
     .prompt([
@@ -22,8 +25,56 @@ function operation() {
     ])
     .then((answer) => {
       const action = answer.action;
-      console.log("Ação escolhida:", action);
+
+      if (action === 'Criar conta') {
+        createAccount();
+      }
+
+
     });
 }
 
-operation();
+//create account
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'));
+  console.log(chalk.green('Defina as opções da sua conta a seguir:'));
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer.prompt([
+    {
+      name: 'accountName',
+      message: 'Digite um nome para a sua conta:',
+    },
+  ]).then((answer) => {
+    const accountName = answer['accountName'];
+    
+    console.info(accountName);
+
+    if (!fs.existsSync('accounts')) {
+      fs.mkdirSync('accounts');
+    }
+
+    if (fs.existsSync(`accounts/${accountName}.json`)) {
+      console.log(
+        chalk.red('Esta conta já existe, escolha outro nome!')
+      );
+      buildAccount();
+      return;
+    }
+
+    fs.writeFileSync(
+      `accounts/${accountName}.json`,
+      '{"balance": 0}',
+      function (err) {
+        console.log(err);
+      }
+    );
+    console.log(chalk.green('Parabéns, sua conta foi criada!'));
+    operation();
+    
+  }).catch((error) => {
+    console.error(chalk.red('Erro ao criar conta:', error));
+  });
+}
