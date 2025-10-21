22f9dc549a190e9573c87ad86624b2726e74682d
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-10-21 13:02:02 -0300

    feature: saldo

--- 5_accounts/index.js
+++ 5_accounts/index.js
@@ -158,3 +158,22 @@ function getAccount(accountName) {
   });
   return JSON.parse(accountJSON);
 }
+
+function getAccountBalance(accountName) {
+ inquirer.prompt([
+    {
+      name: 'accountName',
+      message: 'Qual o nome da sua conta?',
+    },
+  ]).then((answer) => {
+    const accountName = answer['accountName'];
+    if (!checkAccount(accountName)) {
+      return getAccountBalance();
+    }
+    const accountData = getAccount(accountName);
+    console.log(chalk.bgBlue.black(`Olá, o saldo da sua conta é R$${accountData.balance}`));
+    operation();
+  }
+  ).catch((err) => console.error(err));
+}
+  
