e284c7b60cb51d927ee1ec1481b3b88b872626aa
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-10-23 10:11:53 -0300

    feature:parametros via url

---
+++ 6_express/6_param_url/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const path = require('path');
+
+const basePath = path.join(__dirname, 'templates');
+
+app.get('/users/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    //leitura da tabela users, no banco de dados
+    console.log(`Estamos buscando pelo usuário: ${id}`);
+
+    res.sendFile(`${basePath}/users.html`);
+});
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
