03b681d17e7a6eb8fd786a8f26582fdbc7a45f6e
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-11-01 13:36:58 -0300

    feature:toughts

--- 11_toughts/controllers/ToughtsController.js
+++ 11_toughts/controllers/ToughtsController.js
@@ -7,7 +7,51 @@ module.exports = class ToughtsController {
     }
 
     static async dashboard(req, res) {
-        res.render('toughts/dashboard');
+
+        const userId = req.session.userid;
+        const user = await User.findOne(
+            {where: {id: userId},
+            include: Tought,
+            plain: true}
+        );
+
+        //Verificação se o usuario existe
+        if(!user) {
+            res.redirect('/login');
+            return;
+        }
+        // Converte cada instância de Tought em um objeto simples, pegando apenas os dados (dataValues)
+        const toughts = user.Toughts.map((result)=> result.dataValues);
+
+        let emprtyToughts = false;
+
+        if(toughts.length === 0) {
+            emprtyToughts = true;
+        }
+
+        res.render('toughts/dashboard', {toughts, emprtyToughts});
+    }
+
+    static async deleteTought(req, res) {
+        //pega o id do pensamento
+        const id = req.body.id;
+
+        //pega o id do usuario logado
+        const userId = req.session.userid;
+
+        try {
+            //deleta o pensamento onde o id do pensamento e o id do usuario batem
+            await Tought.destroy({where: {id: id, UserId: userId}});
+
+            // Redireciona o usuário de volta para o dashboard
+            req.flash('message', 'Pensamento deletado com sucesso!');
+
+            res.redirect('/toughts/dashboard');
+        } catch (error) {
+            console.error(error);
+            req.flash('error', 'Erro ao deletar pensamento.');
+            res.redirect('/toughts/dashboard');
+        }
     }
 
     static async createTought(req, res) {
--- 11_toughts/routes/toughtsRoutes.js
+++ 11_toughts/routes/toughtsRoutes.js
@@ -9,6 +9,7 @@ const  checkAuth  = require('../helpers/auth').checkAuth
 router.get('/dashboard', checkAuth,ToughtsController.dashboard);
 router.get('/add', checkAuth,ToughtsController.createTought);
 router.post('/add', checkAuth,ToughtsController.createToughtSave);
+router.post('/delete', checkAuth,ToughtsController.deleteTought);
 router.get('/', ToughtsController.showToughts);
 
 
