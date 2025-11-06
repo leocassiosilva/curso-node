143da87e4b36028dd4dc4c288f69b854339cbac5
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-11-06 10:05:32 -0300

    feature:mongo

---
+++ 12_mongo_db/5_deletando_dados/models/Product.js
@@ -0,0 +1,45 @@
+const conn = require('../db/conn');
+const {ObjectId} = require('mongodb');
+class Product {
+
+    constructor(name, price, description, image) {
+        this.name = name;
+        this.image = image
+        this.price = price;
+        this.description = description;
+    }
+
+    save() {
+        const product = conn.db().collection('products').insertOne({
+            name: this.name,
+            price: this.price,
+            description: this.description,
+            image: this.image
+        });
+        return product;
+    }
+
+    static getProducts() {
+        const products = conn.db().collection('products').find().toArray()
+
+        return products
+    }
+    
+    static async getProductById(id) {
+        const product = await conn
+        .db()
+        .collection('products')
+        .findOne({ _id: new ObjectId(id) });
+        return product;
+    }
+    static async removeById(id) {
+        await conn
+        .db()
+        .collection('products')
+        .deleteOne({ _id: new ObjectId(id) });
+        return;
+    }
+
+}
+
+module.exports = Product;
---
+++ 12_mongo_db/5_deletando_dados/routes/ProductsRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const productsController = require('../controllers/ProductController');
+
+// Route to create a new product
+router.get('/create', productsController.createProduct);
+
+// Route to handle product creation form submission
+router.post("/create", productsController.createProductPost);
+
+//Remove product
+router.post("/remove/:id", productsController.removeProductPost);
+
+// Route to product
+router.get('/:id', productsController.getProductById);
+
+// Route to show products
+router.get('/', productsController.showProducts);
+
+module.exports = router;
