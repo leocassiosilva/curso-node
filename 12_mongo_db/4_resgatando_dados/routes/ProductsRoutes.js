90315477bdf38f4e605725ad974f7b60335ff494
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-11-06 09:03:18 -0300

    feature:mongo

---
+++ 12_mongo_db/4_resgatando_dados/routes/ProductsRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const productsController = require('../controllers/ProductController');
+
+// Route to create a new product
+router.get('/create', productsController.createProduct);
+
+// Route to handle product creation form submission
+router.post("/create", productsController.createProductPost);
+
+// Route to product
+router.get('/:id', productsController.getProductById);
+
+// Route to show products
+router.get('/', productsController.showProducts);
+
+module.exports = router;
