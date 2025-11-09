965e2b5ac52597908c5bd7b3afe79fa4cf09bea8
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-11-09 14:21:15 -0300

    feature:mongoose

---
+++ 13_mongoose/1_instalacao/controllers/ProductController.js
@@ -0,0 +1,57 @@
+const Product = require('../models/Product');
+
+module.exports = class ProductsController {
+    static async showProducts(req, res) {
+        const products = await Product.getProducts();
+        console.log(products);
+        res.render('products/all', { products });
+    }
+
+    static async getProductById(req, res) {
+        const id = req.params.id;
+        const product = await Product.getProductById(id);
+        res.render('products/product', { product });
+    }
+
+    static async editProductById(req, res) {
+        const id = req.params.id;
+        const product = await Product.getProductById(id);
+        res.render('products/edit', { product });
+    }
+
+    static async editProductByIdSave(req, res) {
+        const id = req.body.id;
+        const name = req.body.name;
+        const price = req.body.price;
+        const description = req.body.description;
+        const image = req.body.image;
+
+        const product = new Product(name, price, description, image);
+
+        await product.updateProduct(id);
+        res.redirect('/products');
+    }
+
+
+        
+    static async removeProductPost(req, res) {
+        const id = req.body.id;
+        await Product.removeById(id);
+        res.redirect('/products');
+    }
+
+    static async createProduct(req, res) {
+        res.render('products/create')
+    }
+
+    static async createProductPost(req, res) {
+        const name = req.body.name;
+        const price = req.body.price;
+        const description = req.body.description;
+        const image = req.body.image;
+
+        const product = new Product(name, price, description, image);
+        await product.save();
+        res.redirect('/products');
+    }
+}
