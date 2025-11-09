5d698ebf44be7331866b87694aa0662d2265c0c4
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-11-09 14:55:42 -0300

    inserindo dados no banco com mongoose

---
+++ 13_mongoose/3_inserindo_dados/routes/ProductsRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const productsController = require('../controllers/ProductController');
+
+// Route to create a new product
+router.get('/create', productsController.createProduct);
+
+// Route to handle product creation form submission
+router.post("/create", productsController.createProductPost);
+
+// //Remove product
+// router.post("/remove/:id", productsController.removeProductPost);
+
+// // Route to product
+// router.post('/edit', productsController.editProductByIdSave);
+
+// // Route to product
+// router.get('/edit/:id', productsController.editProductById);
+
+// // Route to product
+// router.get('/:id', productsController.getProductById);
+
+// // Route to show products
+// router.get('/', productsController.showProducts);
+
+module.exports = router;
