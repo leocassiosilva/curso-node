3890cbff594e8a6ab3814ae64896ec471c5ea6a9
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-11-21 20:39:33 -0300

    feature:fazendo login ao se cadastrar e utilizando useNavigate

--- 15_get_a_pet/frontend/src/hooks/useAuth.js
+++ 15_get_a_pet/frontend/src/hooks/useAuth.js
@@ -3,27 +3,43 @@ import api from '../utils/api';
 
 // hook
 import useFlashMessage from './useFlashMessage';
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function useAuth() {
-    const {setFlashMessage} = useFlashMessage();
+    const { setFlashMessage } = useFlashMessage();
+    const [authenticated, setAuthenticated] = useState(false);
 
+    // Hook correto para navegação (React Router v6+)
+    const navigate = useNavigate();
 
     async function register(user) {
         let msgText = 'Cadastro realizado com sucesso!';
         let msgType = 'success';
         
-        try{
+        try {
             const data = await api.post('/users/register', user).then((response) => {
-                return response.data
-            })
+                return response.data;
+            });
+
+            await authUser(data);
+
         } catch (error) {
             console.log(error);
-            msgText = error.response.data.message;
+            msgText = error.response?.data?.message || 'Erro ao cadastrar';
             msgType = 'error';
         }
 
         setFlashMessage(msgText, msgType);
     }
-    return {register};
+
+    async function authUser(data) {
+        setAuthenticated(true);
+        localStorage.setItem('token', JSON.stringify(data.token));
+
+        // Forma correta no React Router v6
+        navigate('/');
+    }
+
+    return { register, authUser };
 }
